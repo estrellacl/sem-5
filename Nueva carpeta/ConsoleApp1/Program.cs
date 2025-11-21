375ae9fdea7f71962cf2452d23e4f7caf5c8d49c
estrellacl <N00477869@upn.pe>
2025-11-21 10:33:47 -0500

    Add files via upload

---
+++ Nueva carpeta/ConsoleApp1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numero;
+
+            while (true)
+            {
+                
+                Console.WriteLine("\nIngrese un número decimal con exactamente 1 entero y 2 decimales :");
+                numero = Console.ReadLine().Trim();
+
+                //  NO esté vacío
+                if (string.IsNullOrWhiteSpace(numero))
+                {
+                    Console.WriteLine("NO SE PERMITEN VACÍOS.\n");
+                    continue;
+                }
+
+                // contenga un punto
+                if (!numero.Contains("."))
+                {
+                    Console.WriteLine("Debe contener un punto decimal.\n");
+                    continue;
+                }
+
+                // solo haya UN punto
+                if (numero.IndexOf('.') != numero.LastIndexOf('.'))
+                {
+                    Console.WriteLine("El número solo puede tener un punto decimal.\n");
+                    continue;
+                }
+
+                // Separar en partes antes y después del punto
+                string[] partes = numero.Split('.');
+
+                //  parte entera y parte decimal
+                if (partes.Length != 2)
+                {
+                    Console.WriteLine("Formato incorrecto.\n");
+                    continue;
+                }
+
+                string entero = partes[0];
+                string decimales = partes[1];
+
+                // la parte entera tenga  1 dígito
+                if (entero.Length != 1 || !char.IsDigit(entero[0]))
+                {
+                    Console.WriteLine("El entero debe tener exactamente 1 dígito.\n");
+                    continue;
+                }
+
+                //  la parte decimal tenga  2 dígitos
+                if (decimales.Length != 2 || !char.IsDigit(decimales[0]) || !char.IsDigit(decimales[1]))
+                {
+                    Console.WriteLine("Los decimales deben tener exactamente 2 dígitos.\n");
+                    continue;
+                }
+
+                // Si todo es válido, salimos del ciclo
+                break;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"\nNúmero válido ingresado: {numero}");
+            Console.ResetColor();
+
+
+
+
+        }
+    }
+}
---
+++ Nueva carpeta/t14/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nom1, nom2;
+
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("\nIngrese nombre 1 :");
+                nom1 = Console.ReadLine().Trim();
+                if (!string.IsNullOrWhiteSpace(nom1))
+                    break;
+                else Console.WriteLine("NO SE PERMITEN VACIOS.\n");
+            }
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("\nIngrese nombre 2 :");
+                nom2 = Console.ReadLine().Trim();
+                if (!string.IsNullOrWhiteSpace(nom2))
+                    break;
+                else Console.WriteLine("NO SE PERMITEN VACIOS.\n");
+            }
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Cantidad de caracteres nombre 1 :" + nom1.Length);
+            Console.WriteLine("Cantidad de carcateres nombre 2 :" + nom2.Length);
+            Console.WriteLine("Nombre 1 en mayusuculas:" + nom1.ToUpper());
+            Console.WriteLine("Nombre 2 en minuscula:" + nom2.ToLower());
+
+            if (nom1.CompareTo(nom2) == 0)
+                Console.WriteLine("Nombres son iguales");
+            else
+                Console.WriteLine("Nombres son diferentes");
+
+
+
+
+            if (nom1.Contains("an"))
+                Console.WriteLine("Si existe 'an'");
+            else
+                Console.WriteLine("No existe 'an");
+
+
+
+            if (nom1.IndexOf("a") != -1)
+                Console.WriteLine("1°pos:" + nom1.IndexOf("a"));
+            else
+                Console.WriteLine("No existe 'a'");
+
+            if (nom2.LastIndexOf("a") != -1)
+                Console.WriteLine("1°pos:" + nom2.LastIndexOf("a"));
+            else
+                Console.WriteLine("No existe 'a'");
+
+            Console.WriteLine("Insertando UPN:" + nom1.Insert(0, "UPN"));
+            Console.ResetColor();
+            Console.WriteLine("Insertando SISTEMAS:" + nom2.Insert(nom2.Length, "SISTEMAS"));
+
+            if (nom1.Length > 5)
+                Console.WriteLine("Eliminado desde 5° pos" + nom1.Remove(5));
+            else
+                Console.WriteLine("Al menos debe tener 5 caracteres");
+            Console.BackgroundColor = ConsoleColor.Red;
+
+            if (nom1.Contains("a"))
+                Console.WriteLine("Remplazando a po @:" + nom1.Replace("a", "@"));
+            else
+                Console.WriteLine("No existe 'a' ");
+            Console.ResetColor();
+
+            if (nom1.Contains("a"))
+            {
+                Console.Write("Division de partes:");
+                string[] partes = nom1.Split('a');
+                for (int i = 0; i < partes.Length; i++)
+                    Console.Write(partes[i] + "  ->");
+
+
+            }
+            else
+                Console.WriteLine("No existe 'a' ");
+
+            if (nom1.Length > 4)
+                Console.WriteLine(" \n Sub cadena:" + nom1.Substring(4));
+            else
+                Console.WriteLine("Al menos debe de tener 4 caracteres");
+
+            char[] nom = nom1.ToCharArray();
+            for (int i = 0; i < nom.Length; i++)
+                Console.WriteLine(nom[i]);
+
+            Console.WriteLine("Reversa:");
+            for (int i = nom.Length - 1; i >= 0; i--)
+                Console.Write(nom[1]);
+
+            Console.WriteLine( "\nordenando:");
+            Array.Sort(nom);
+            for (int i = 0;i< nom.Length ; i++)
+                Console.Write(nom[1]);
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
